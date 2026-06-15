9ee54c5410a3bfe0443c3ec9b794ec7173743943
AngelDev06 <aggelos.tserkezhs@gmail.com>
2026-06-15 17:06:27 +0300

    fixed R formatter timeout issues

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -9,12 +9,13 @@ local options = {
     c = { "clang-format" },
     cpp = { "clang-format" },
     cmake = { "cmake_format" },
-    r = { "styler" },
+    r = { "styler", timeout_ms = 15000 },
   },
   default_format_opts = {
     lsp_format = "fallback",
+    timeout_ms = 5000,
   },
-  format_on_save = { timeout_ms = 5000 },
+  format_on_save = {},
   formatters = {
     shfmt = {
       prepend_args = { "-i", "2" },
